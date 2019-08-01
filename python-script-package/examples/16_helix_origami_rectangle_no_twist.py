9674291232bc9d12abe51d0e0b7e48821ebb8fb6
dave-doty <pexatus@gmail.com>
2019-08-01 09:32:43 -0700

    modified example scripts for testing

--- python-script-package/examples/16_helix_origami_rectangle_no_twist.py
+++ python-script-package/examples/16_helix_origami_rectangle_no_twist.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     rect_num_helices = 16
     rect_num_cols = 24  # XXX: ensure num_cols is even since we divide it by 2
     rect_design = rect.create(num_helices=rect_num_helices, num_cols=rect_num_cols,
-                              assign_seq=False, twist_correction_deletion_spacing=3)
-    rect_design.write_to_file("output_designs/16_helix_rectangle_no_seq_no_twist.dna")
+                              assign_seq=True, twist_correction_deletion_spacing=3)
+    rect_design.write_to_file("output_designs/16_helix_rectangle_no_twist.dna")
 
 # M13 in on helix 1 is (going left-to-right, which is 3'-to-5')
 # GATTTTGTGAGTAGAA
--- python-script-package/examples/2_staple_2_helix_origami_6bases_wide.py
+++ python-script-package/examples/2_staple_2_helix_origami_6bases_wide.py
@@ -1,22 +1,24 @@
 import scadnano as sc
 
 if __name__ == "__main__":
-    helices = [sc.Helix(0, 32), sc.Helix(1, 32)]
-    stap_left_ss1 = sc.Substrand(1, sc.right, 0, 16)
-    stap_left_ss0 = sc.Substrand(0, sc.left, 0, 16)
-    stap_right_ss0 = sc.Substrand(0, sc.left, 16, 32)
-    stap_right_ss1 = sc.Substrand(1, sc.right, 16, 32)
-    scaf_ss1_left = sc.Substrand(1, sc.left, 0, 16)
-    scaf_ss0 = sc.Substrand(0, sc.right, 0, 32)
-    scaf_ss1_right = sc.Substrand(1, sc.left, 16, 32)
+    width=6
+    width_h=width//2
+    helices = [sc.Helix(0, width), sc.Helix(1, width)]
+    stap_left_ss1 = sc.Substrand(1, sc.right, 0, width_h)
+    stap_left_ss0 = sc.Substrand(0, sc.left, 0, width_h)
+    stap_right_ss0 = sc.Substrand(0, sc.left, width_h, width)
+    stap_right_ss1 = sc.Substrand(1, sc.right, width_h, width)
+    scaf_ss1_left = sc.Substrand(1, sc.left, 0, width_h)
+    scaf_ss0 = sc.Substrand(0, sc.right, 0, width)
+    scaf_ss1_right = sc.Substrand(1, sc.left, width_h, width)
     stap_left = sc.Strand([stap_left_ss1, stap_left_ss0])
     stap_right = sc.Strand([stap_right_ss0, stap_right_ss1])
     scaf = sc.Strand([scaf_ss1_left, scaf_ss0, scaf_ss1_right], color=sc.default_scaffold_color)
     strands = [stap_left, stap_right, scaf]
     design = sc.DNADesign(helices=helices, strands=strands, grid=sc.square)
-    design.add_deletion(helix_idx=0, offset=12)
-    design.add_deletion(helix_idx=0, offset=24)
-    design.add_deletion(helix_idx=1, offset=12)
-    design.add_deletion(helix_idx=1, offset=24)
-    design.assign_dna(scaf, 'AACT'*16)
-    design.write_to_file("output_designs/2_staple_2_helix_origami.dna")
+    design.add_deletion(helix_idx=0, offset=1)
+    design.add_deletion(helix_idx=0, offset=4)
+    design.add_deletion(helix_idx=1, offset=1)
+    design.add_deletion(helix_idx=1, offset=4)
+    design.assign_dna(scaf, 'AACATCGT')
+    design.write_to_file("output_designs/2_staple_2_helix_origami_small.dna")
