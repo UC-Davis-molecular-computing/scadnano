acb1cf5cf2fc6fd03ca89d09bec2ba62a28ace00
dave-doty <pexatus@gmail.com>
2019-08-14 00:28:15 -0700

    added scripts for helping compile and upload

--- compile.sh
+++ compile.sh
@@ -2,8 +2,14 @@
 echo "running webdev directly, but if it fails, try first running" 
 echo "  pub.bat global activate webdev"
 webdev.bat build
-# packages directory appears not to be needed for the production version of the app
+
+# most packages appear not to be needed for the production version of the app
 # but keep an eye out for bugs related to this and possibly use it if needed (it's 46 MB last I checked)
-rm -rf build/packages 
-rm build/external-libs/simpledrag.js
-rm build/external-libs/svg-pan-zoom.js
+for directory in build/packages/*; do
+    if [[ "$directory" = "build/packages/codemirror" || "$directory" = "build/packages/split" ]]; then
+        echo "leaving $directory alone since it is needed in the app"
+    else
+        echo "deleting $directory"
+        rm -rf "$directory"
+    fi
+done
---
+++ up.sh
@@ -0,0 +1,4 @@
+scp build/* doty@set.cs.ucdavis.edu:public_html/scadnano/
+scp build/examples/* doty@set.cs.ucdavis.edu:public_html/scadnano/examples/
+scp build/external-libs/* doty@set.cs.ucdavis.edu:public_html/scadnano/external-libs/
+scp build/images/* doty@set.cs.ucdavis.edu:public_html/scadnano/images/
