c95525d59704a7d6bc5cddc6c09949f6504a590a
dave-doty <doty@ucdavis.edu>
2019-12-02 08:59:28 -0800

    factored svg filters out of DesignMain view into its own file

---
+++ lib/src/view/svg_filters.dart
@@ -0,0 +1,32 @@
+import 'dart:svg' as svg;
+
+add_shadow_filter(svg.SvgSvgElement elt) {
+  // https://stackoverflow.com/a/6094674
+  var drop_shadow = [
+    svg.FEGaussianBlurElement()
+      ..attributes = {
+//          'in': 'SourceAlpha',
+        'stdDeviation': '2.5',
+      },
+    svg.FEMergeElement()
+      ..children = [
+        svg.FEMergeNodeElement(),
+        svg.FEMergeNodeElement()..attributes = {'in': "SourceGraphic"},
+      ],
+  ];
+
+  var defns = svg.DefsElement()
+    ..children = [
+      svg.FilterElement()
+        ..children = drop_shadow
+        ..attributes = {
+          'id': 'shadow',
+          'filterUnits': 'userSpaceOnUse',
+          'x': '-300%',
+          'y': '-300%',
+          'width': '900%',
+          'height': '900%',
+        }
+    ];
+  elt.children.add(defns);
+}
