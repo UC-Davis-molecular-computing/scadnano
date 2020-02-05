84469acf85f841e7a37647eee5dc62513ca1592a
Dave Doty <doty@ucdavis.edu>
2020-02-04 18:02:09 -0800

    added CNAME

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,9 +1,15 @@
 # from master branch, do this
 
-pub get # might not be needed
+#TODO: don't know how to prevent this check from printing error message to the screen in case pub isn't found
+if type pub > /dev/null; then
+  PUB=pub
+  echo "Using pub as dart pub command"
+else 
+  # on Windows the pub command is called pub.bat
+  PUB=pub.bat
+  echo "Using pub.bat as dart pub command"
+fi
+
+$PUB get # might not be needed
 peanut
-git checkout gh-pages
-echo "scadnano.org" > CNAME
-git commit -m "create CNAME"
-git push
-git checkout master
+git push origin --set-upstream gh-pages
