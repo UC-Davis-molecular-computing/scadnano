ad68173bd6c5a8a562be3b0ae4404ebbc7043b7e
Dave Doty <doty@ucdavis.edu>
2020-02-13 09:26:54 -0800

    fixed #173

--- lib/src/middleware/load_file.dart
+++ lib/src/middleware/load_file.dart
@@ -16,24 +16,26 @@ load_file_middleware(Store<AppState> store, action, NextDispatcher next) {
 
     // re-center if necessary
     if (store.state.ui_state.autofit && store.state.dna_design != null) {
-      Point<num> pan_side = Point<num>(50, 50);
-      Point<num> pan_main = Point<num>(100, 50);
-      if (store.state.dna_design.helices.length > 0) {
-        // center at smallest-index helix
-        var helices = store.state.dna_design.helices.values.toList();
-        var first_helix = helices.first;
-        for (var helix in helices) {
-          if (first_helix.idx > helix.idx) {
-            first_helix = helix;
-          }
-        }
-        pan_main -= first_helix.svg_position;
-        pan_side -= util.position3d_to_side_view_svg(first_helix.position3d());
-      }
+//      Point<num> pan_side = Point<num>(50, 50);
+//      Point<num> pan_main = Point<num>(100, 50);
+//      if (store.state.dna_design.helices.length > 0) {
+//        // center at smallest-index helix
+//        var helices = store.state.dna_design.helices.values.toList();
+//        var first_helix = helices.first;
+//        for (var helix in helices) {
+//          if (first_helix.idx > helix.idx) {
+//            first_helix = helix;
+//          }
+//        }
+//        pan_main -= first_helix.svg_position;
+//        pan_side -= util.position3d_to_side_view_svg(first_helix.position3d());
+//      }
+//      print('pan_side: $pan_side');
+//      print('pan_main: $pan_main');
 
-      util.set_pan_side(pan_side);
-      util.set_pan_main(pan_main);
-//      util.fit_and_center(); // doesn't work like I had hoped
+//      util.set_pan_side(pan_side);
+//      util.set_pan_main(pan_main);
+      util.fit_and_center();
     }
   }
 }
