820f22a11df6e4ae57cde12cd670aa0c8de4bcaf
David Doty <doty@ucdavis.edu>
2020-05-27 13:44:37 -0700

    bumped version to 0.7.0 after breaking change with default grid type

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -26,7 +26,7 @@ const KEY_CODE_LOOPOUT_CONVERT = KeyCode.L;
 
 const String INITIAL_VERSION = "0.1.0";
 
-const String CURRENT_VERSION = "0.6.7";
+const String CURRENT_VERSION = "0.7.0";
 
 const String DNA_BASE_WILDCARD = '?';
 
