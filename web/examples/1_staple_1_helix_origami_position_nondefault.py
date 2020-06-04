c9b9d06fe2fd0145af4c9192fb476b827ac71cda
David Doty <doty@ucdavis.edu>
2020-06-03 17:50:37 -0700

    updated examples with new data for pitch,roll,yaw being direct fields of Helix and not in Helix.position

--- web/examples/1_staple_1_helix_origami_position_nondefault.py
+++ web/examples/1_staple_1_helix_origami_position_nondefault.py
@@ -1,9 +1,10 @@
 import scadnano as sc
 
+
 def main():
     length = 9
     helices = [
-        sc.Helix(max_offset=length, major_ticks=[2, 5], position3d=sc.Position3D(x=3, y=2, z=1, pitch=90))]
+        sc.Helix(max_offset=length, major_ticks=[2, 5], position3d=sc.Position3D(x=3, y=2, z=1), pitch=90)]
     stap_ss = sc.Domain(0, sc.forward, 0, length)
     scaf_ss = sc.Domain(0, sc.reverse, 0, length)
     stap = sc.Strand([stap_ss])
@@ -13,6 +14,7 @@ def main():
     design.assign_dna(scaf, 'AACT' * (length // 4))
 
     return design
+
 
 if not sc.in_browser() and __name__ == '__main__':
     design = main()
