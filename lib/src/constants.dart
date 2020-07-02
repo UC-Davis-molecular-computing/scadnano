85e333a94dc0fad588c899ef4f709a3e279a0d18
David Doty <doty@ucdavis.edu>
2020-07-01 18:19:06 -0700

    bumped version

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -4,7 +4,7 @@ import 'package:platform_detect/platform_detect.dart';
 
 import 'state/grid.dart';
 
-const String CURRENT_VERSION = "0.9.3-1";
+const String CURRENT_VERSION = "0.9.3-2";
 const String INITIAL_VERSION = "0.1.0";
 
 const BUG_REPORT_URL = 'https://github.com/UC-Davis-molecular-computing/scadnano/issues';
