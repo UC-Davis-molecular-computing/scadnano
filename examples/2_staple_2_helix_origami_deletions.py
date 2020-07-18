6a65cd9272fc4c95ac641b6ebfa5e65eb9c8b129
dave-doty <dave-doty@users.noreply.github.com>
2020-07-18 18:35:57 +0000

    deploy: 3d5a61e25545e5251eed63e4873d40fd24894ccb

--- examples/2_staple_2_helix_origami_deletions.py
+++ examples/2_staple_2_helix_origami_deletions.py
@@ -2,13 +2,13 @@ import scadnano as sc
 
 
 def main():
-    stap_left_ss1 = sc.Domain(1, sc.forward, 0, 16)
-    stap_left_ss0 = sc.Domain(0, sc.reverse, 0, 16)
-    stap_right_ss0 = sc.Domain(0, sc.reverse, 16, 32)
-    stap_right_ss1 = sc.Domain(1, sc.forward, 16, 32)
-    scaf_ss1_left = sc.Domain(1, sc.reverse, 0, 16)
-    scaf_ss0 = sc.Domain(0, sc.forward, 0, 32)
-    scaf_ss1_right = sc.Domain(1, sc.reverse, 16, 32)
+    stap_left_ss1 = sc.Domain(1, True, 0, 16)
+    stap_left_ss0 = sc.Domain(0, False, 0, 16)
+    stap_right_ss0 = sc.Domain(0, False, 16, 32)
+    stap_right_ss1 = sc.Domain(1, True, 16, 32)
+    scaf_ss1_left = sc.Domain(1, False, 0, 16)
+    scaf_ss0 = sc.Domain(0, True, 0, 32)
+    scaf_ss1_right = sc.Domain(1, False, 16, 32)
     stap_left = sc.Strand([stap_left_ss1, stap_left_ss0])
     stap_right = sc.Strand([stap_right_ss0, stap_right_ss1])
     scaf = sc.Strand([scaf_ss1_left, scaf_ss0, scaf_ss1_right], color=sc.default_scaffold_color)
