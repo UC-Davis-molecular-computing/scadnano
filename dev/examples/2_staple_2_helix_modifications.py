8b39848280c6aafcc2d9ef971711b4c0da350967
UnHumbleBen <UnHumbleBen@users.noreply.github.com>
2020-07-18 18:45:36 +0000

    deploy: 56bd71d653302ed933f2124f703f7380d172b2a9

--- dev/examples/2_staple_2_helix_modifications.py
+++ dev/examples/2_staple_2_helix_modifications.py
@@ -3,13 +3,13 @@ import modifications as mod
 import dataclasses
 
 def main():
-    stap_left_ss1 = sc.Domain(1, sc.forward, 0, 16)
-    stap_left_ss0 = sc.Domain(0, sc.reverse, 0, 16)
-    stap_right_ss0 = sc.Domain(0, sc.reverse, 16, 32)
-    stap_right_ss1 = sc.Domain(1, sc.forward, 16, 32)
-    scaf_ss1_left = sc.Domain(1, sc.reverse, 0, 16)
-    scaf_ss0 = sc.Domain(0, sc.forward, 0, 32)
-    scaf_ss1_right = sc.Domain(1, sc.reverse, 16, 32)
+    stap_left_ss1 = sc.Domain(1, True, 0, 16)
+    stap_left_ss0 = sc.Domain(0, False, 0, 16)
+    stap_right_ss0 = sc.Domain(0, False, 16, 32)
+    stap_right_ss1 = sc.Domain(1, True, 16, 32)
+    scaf_ss1_left = sc.Domain(1, False, 0, 16)
+    scaf_ss0 = sc.Domain(0, True, 0, 32)
+    scaf_ss1_right = sc.Domain(1, False, 16, 32)
     stap_left = sc.Strand([stap_left_ss1, stap_left_ss0])
     stap_right = sc.Strand([stap_right_ss0, stap_right_ss1])
     scaf = sc.Strand([scaf_ss1_left, scaf_ss0, scaf_ss1_right], color=sc.default_scaffold_color)
@@ -39,6 +39,11 @@ def main():
     stap_right.set_modification_5p(mod.cy5_5p)
     stap_right.set_modification_internal(5, mod.cy3_int)
     stap_right.set_modification_3p(mod.biotin_3p)
+
+    scaf.set_modification_5p(mod.biotin_5p)
+    scaf.set_modification_3p(mod.cy3_3p)
+    scaf.set_modification_internal(5, mod.cy5_int)
+    scaf.set_modification_internal(32, mod.cy3_int)
 
     return design
 
