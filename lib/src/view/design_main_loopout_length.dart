e77e54d820f5b24e12df65c21e95ce0d724eb0e9
rishabhmudradi <rishabh.mudradi@gmail.com>
2020-08-09 17:45:58 -0700

    Removing loopout parameter
    Forgot to change this in my last commit.

--- lib/src/view/design_main_loopout_length.dart
+++ lib/src/view/design_main_loopout_length.dart
@@ -33,26 +33,26 @@ class DesignMainLoopoutComponent extends UiComponent2<DesignMainLoopoutProps> wi
   @override
   render() {
     List<ReactElement> loopout_length_elts = [];
-    loopout_length_elts.add(this._loopout_length(props.loopout));
+    loopout_length_elts.add(this._loopout_length());
     return (Dom.g()..className = 'strand-loopout-length')(loopout_length_elts);
   }
 
-  ReactElement _loopout_length(Loopout loopout) {
+  ReactElement _loopout_length() {
     var start_offset = '50%';
     var dy = '${0.1 * props.geometry.base_width_svg}';
 
     Map<String, dynamic> style_map = {'fontSize': '${9}px'};
-    var loopout_length = loopout.loopout_length.toString().split('').reversed.join('');
+    var loopout_length = props.loopout.loopout_length.toString().split('').reversed.join('');
 
     SvgProps text_path_props = (Dom.textPath()
       ..className = constants.css_selector_loopout_length
-      ..xlinkHref = '#${loopout.id()}'
+      ..xlinkHref = '#${props.loopout.id()}'
       ..startOffset = start_offset
       ..style = style_map);
     return (Dom.text()
       ..rotate = 180
       ..key = 'loopout-length-'
-          'H${loopout.id()},'
+          'H${props.loopout.id()},'
       ..dy = dy)(text_path_props(loopout_length));
   }
 }
