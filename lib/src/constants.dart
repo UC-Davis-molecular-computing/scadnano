466202aa2b17cd2a4611daca1e52e5d11b0cc8fd
David Doty <doty@ucdavis.edu>
2020-08-11 09:32:23 -0700

    changed default inter-helix gap to be 1 nm (previously was 0.5 nm)

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -120,7 +120,7 @@ const default_rise_per_base_pair = 0.332;
 const default_helix_radius = 1.0;
 const default_bases_per_turn = 10.5;
 const default_minor_groove_angle = 150.0;
-const default_inter_helix_gap = 0.5;
+const default_inter_helix_gap = 1.0;
 
 /////////////////////////////////////////////////////////////
 // JSON keys
