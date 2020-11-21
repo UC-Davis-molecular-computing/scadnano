13264ab67f7ab66399ea25dce604d50882415e93
Benjamin Lee <bnllee@ucdavis.edu>
2020-11-21 07:55:11 -0800

    v0.13.0

---
+++ v0.13.0/examples/6_helix_origami_rectangle_helices_out_of_order.py
@@ -0,0 +1,13 @@
+import origami_rectangle as rect
+import scadnano as sc
+
+
+def create_design():
+    design = rect.create(num_helices=6, num_cols=10, nick_pattern=rect.staggered, twist_correction_deletion_spacing=3)
+    design.set_helices_view_order([5,4,3,2,1,0])
+    return design
+
+
+if __name__ == '__main__':
+    design = create_design()
+    design.write_scadnano_file(directory='output_designs')
