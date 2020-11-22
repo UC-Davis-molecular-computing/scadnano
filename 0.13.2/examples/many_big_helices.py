8dfff9bef8ae7beb210c017a35f7fd21bf0b43a6
dave-doty <dave-doty@users.noreply.github.com>
2020-11-22 20:15:02 +0000

    deploy: a57200d783f4a1d3cfb8f1e682d0cbb2a5e33f05

---
+++ 0.13.2/examples/many_big_helices.py
@@ -0,0 +1,16 @@
+import scadnano as sc
+
+
+def create_design():
+    helices = []
+    num_helices, max_bases = 30, 2000
+    for _ in range(num_helices):
+        helices.append(sc.Helix(max_bases))
+    design = sc.Design(helices=helices, strands=[], grid=sc.square)
+
+    return design
+
+
+if __name__ == '__main__':
+    design = create_design()
+    design.write_scadnano_file(directory='output_designs')
