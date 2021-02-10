b6bed08a5c3640442d97db981d516e30c01dbae6
Benjamin Lee <bnllee@ucdavis.edu>
2021-02-09 17:19:27 -0800

    HOTFIX: Use correct directory names for older versions

--- 0.13.2/external-libs/react-svg-pan-zoom/react_svg_pan_zoom.js
+++
@@ -1,8 +0,0 @@
-console.log('in react_svg_pan_zoom.js');
-import {UncontrolledReactSVGPanZoom} from './react-svg-pan-zoom.min.js';
-
-
-
-// export const UncontrolledReactSVGPanZoom = UncontrolledReactSVGPanZoom;
-// import(UncontrolledReactSVGPanZoom);
-// console.log('UncontrolledReactSVGPanZoom: ' + UncontrolledReactSVGPanZoom);
