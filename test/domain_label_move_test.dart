42511e5fcf478b273836203830cdb08715333f83
Cesar Alonso Guzman Avina <cguzmanavina2001@gmail.com>
2021-07-25 16:58:59 -0700

    Update domain_label_move_test.dart

--- test/domain_label_move_test.dart
+++ test/domain_label_move_test.dart
@@ -2,16 +2,24 @@
 
 import 'dart:convert';
 
+import 'package:built_collection/built_collection.dart';
+import 'package:scadnano/src/actions/actions.dart';
 import 'package:scadnano/src/json_serializable.dart';
+import 'package:scadnano/src/reducers/app_state_reducer.dart';
 import 'package:scadnano/src/reducers/change_loopout_length.dart';
 import 'package:scadnano/src/reducers/delete_reducer.dart';
 import 'package:scadnano/src/reducers/nick_ligate_join_by_crossover_reducers.dart';
 import 'package:scadnano/src/reducers/assign_domain_names_reducer.dart';
+import 'package:scadnano/src/reducers/strands_reducer.dart';
+import 'package:scadnano/src/state/address.dart';
 import 'package:scadnano/src/state/domain.dart';
 import 'package:scadnano/src/state/helix.dart';
 import 'package:scadnano/src/state/grid.dart';
 import 'package:scadnano/src/state/loopout.dart';
 import 'package:scadnano/src/state/select_mode.dart';
+import 'package:scadnano/src/state/selectable.dart';
+import 'package:scadnano/src/state/strand.dart';
+import 'package:scadnano/src/state/strands_move.dart';
 import 'package:test/test.dart';
 
 import 'package:scadnano/src/state/design.dart';
@@ -32,32 +40,25 @@ main() {
       var helices = [Helix(idx: 0, max_offset: 100, grid: Grid.square)];
       var design = Design(helices: helices, grid: Grid.square);
 
-      design = design
-          .strand(0, 0)
-          .move(8)
-          .with_domain_name("A")
-          .cross(1)
-          .move(-8)
-          .with_domain_name("B")
-          .commit();
-      
-
-      var action = actions.StrandsMoveCommit(design);
+      design =
+          design.strand(0, 0).move(8).with_domain_name("A").cross(1).move(-8).with_domain_name("B").commit();
       var state = app_state_from_design(design);
-      var all_strands =
-          strands_move_commit_reducer(design.strands, state, action);
-      expect(all_strands.length, 1);
-
-      var names = [all_strands[0].domains[0].name, all_strands[0].domains[1].name];
-      expect(
-          names,
-          anyOf([
-            equals(["ABC", "ABC*"]),
-            equals(["XYZ*", "XYZ"])
-          ]));
+      print(state.design.default_group().helices_view_order);
+      var new_strand = move_strand(strand: design.strands[0], original_helices_view_order_inverse: state.design.default_group().helices_view_order_inverse, current_group: state.design.groups["default_group"], delta_view_order: state.design.default_group().helices_view_order[0], delta_offset: 10, delta_forward: true);
+      
+      print(design.strands[0]);
+      print(new_strand);
+      // var state = app_state_from_design(design);
+      // StrandsMove strands_move = StrandsMove(
+      //   strands_moving:  design.strands,
+      //   all_strands: design.strands,
+      //   original_address: Address(forward: true, helix_idx: 0, offset: 4),
+      //   helices: state.design.helices,
+      //   groups: state.design.groups,
+      //   original_helices_view_order_inverse: state.design.default_group().helices_view_order_inverse,
+      //   copy: false,
+      // ).rebuild((b) => b..current_address = Address(forward: false, helix_idx: 0, offset: 8).toBuilder());
+      // state = app_state_reducer(state, StrandsMoveCommit(strands_move: strands_move, autopaste: false));
     });
-
-
   });
 }
-
