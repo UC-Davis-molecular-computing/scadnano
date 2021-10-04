7074d70c70147d2db2bb9f1382847acbdd84e869
Benjamin Lee <bnllee@ucdavis.edu>
2021-10-04 03:59:07 -0700

    Add test_2_staple_2_helix_origami_deletions_insertions

--- test/export_cadnano_v2_test.dart
+++ test/export_cadnano_v2_test.dart
@@ -1,3 +1,5 @@
+import 'package:scadnano/src/actions/actions.dart';
+import 'package:scadnano/src/json_serializable.dart';
 import 'package:scadnano/src/state/design.dart';
 import 'package:scadnano/src/state/domain.dart';
 import 'package:scadnano/src/state/grid.dart';
@@ -82,6 +84,49 @@ main() {
       List<Helix> helices = [Helix(idx: 0, max_offset: 32, grid: Grid.square), Helix(idx: 1, max_offset: 32, grid: Grid.square)];
       Design design = Design(helices:helices, grid:Grid.square);
       design = design.strand(0, 0).move(32).cross(1).move(-32).as_scaffold().commit();
+
+      design.to_cadnano_v2_json();
+    });
+
+    test('test_2_staple_2_helix_origami_deletions_insertions', () {
+      List<Helix> helices = [
+        Helix(idx: 0, max_offset: 24, grid: Grid.square),
+        Helix(idx: 1, max_offset: 25, grid: Grid.square),
+      ];
+      Design design = Design(helices: helices, grid: Grid.square);
+
+      // left staple
+      design = design
+        .strand(1, 0)
+        .move(16)
+        .cross(0)
+        .move(-16)
+        .commit();
+
+      // right staple
+      design = design.strand(0, 32).move(-16).cross(1).move(16).commit();
+
+      // scaffold
+      design = design.strand(1, 16)
+        .move(-16)
+        .cross(0)
+        .move(32)
+        .cross(1)
+        .move(-16)
+        .as_scaffold()
+        // also assigns complement to strands other than scaf bound to it
+        // deletions and insertions added to design so they can be added to both strands on a helix
+        .with_sequence('AACGT' * 18)
+        .add_deletion(0, 11)
+        .add_deletion(0, 12)
+        .add_deletion(0, 24)
+        .add_deletion(1, 12)
+        .add_deletion(1, 24)
+        .add_insertion(0, 6, 1)
+        .add_insertion(0, 18, 2)
+        .add_insertion(1, 6, 3)
+        .add_insertion(1, 18, 4)
+        .commit();
 
       design.to_cadnano_v2_json();
     });
