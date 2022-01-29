27aa4af1bf2ada0a15023707be5e6b432a6f4a28
Benjamin Lee <bnllee@ucdavis.edu>
2022-01-29 15:42:04 -0800

    Fixes #724; make stack traces with errors more useful

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -38,9 +38,9 @@ import 'actions/actions.dart' as actions;
 // global variable for whole program
 App app;
 
-const SCADNANO_PROD = String.fromEnvironment('SCADNANO_PROD');
-// const USE_REDUX_DEV_TOOLS = SCADNANO_PROD != 'true';
-const USE_REDUX_DEV_TOOLS = false;
+const SCADNANO_PROD = bool.fromEnvironment('SCADNANO_PROD');
+const USE_REDUX_DEV_TOOLS = !SCADNANO_PROD;
+// const USE_REDUX_DEV_TOOLS = false;
 
 const RUN_TEST_CODE_INSTEAD_OF_APP = false;
 //const RUN_TEST_CODE_INSTEAD_OF_APP = true;
