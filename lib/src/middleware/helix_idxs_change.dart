503cd73fe2a0608b1e58263884d905c2fefef4c6
Ho-Chih <hcma@ucdavis.edu>
2022-07-29 12:31:11 +0800

    add scroll bars and show which index is duplicated

--- lib/src/middleware/helix_idxs_change.dart
+++ lib/src/middleware/helix_idxs_change.dart
@@ -12,11 +12,22 @@ helix_idxs_change_middleware(Store<AppState> store, dynamic action, NextDispatch
     Set<int> existing_idxs = store.state.design.helices.keys.toSet();
     Set<int> old_idxs = action.idx_replacements.keys.toSet();
     Set<int> remaining_idxs = existing_idxs.difference(old_idxs);
+    var new_indices = action.idx_replacements.values.toSet();
 
-    if(action.idx_replacements.values.toSet().length != action.idx_replacements.length) {
-      var msg = 'Duplicates found.';
-      window.alert(msg); 
-      return;  
+    if (new_indices.length != action.idx_replacements.length) {
+      Set<int> intsSoFar = {};
+      List<int> all_helices = action.idx_replacements.values.toList();
+
+      for (var old_index in old_idxs) {
+        var new_index = action.idx_replacements[old_index];
+        if (intsSoFar.contains(new_index)) {
+          var msg = 'First duplicate found for helix ${old_index} with new index ${new_index}. ';
+          window.alert(msg);
+          return;
+        } else {
+          intsSoFar.add(new_index);
+        }
+      }
     }
 
     for (int new_idx in action.idx_replacements.values) {
