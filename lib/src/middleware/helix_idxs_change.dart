1d6c63fbe97897f4c9d392ae9a637a5fdbda3a04
sarahayu <46186848+sarahayu@users.noreply.github.com>
2022-08-12 12:21:19 +0700

    update warning for helix idxs change

--- lib/src/middleware/helix_idxs_change.dart
+++ lib/src/middleware/helix_idxs_change.dart
@@ -15,19 +15,40 @@ helix_idxs_change_middleware(Store<AppState> store, dynamic action, NextDispatch
     var new_indices = action.idx_replacements.values.toSet();
 
     if (new_indices.length != action.idx_replacements.length) {
-      Set<int> intsSoFar = {};
+      Set<int> ints_so_far = {};
       List<int> all_helices = action.idx_replacements.values.toList();
+      Map<int, List<int>> dupe_key_to_idxs = new Map<int, List<int>>();
 
       for (var old_index in old_idxs) {
         var new_index = action.idx_replacements[old_index];
-        if (intsSoFar.contains(new_index)) {
-          var msg = 'First duplicate found for helix ${old_index} with new index ${new_index}. ';
-          window.alert(msg);
-          return;
+        if (ints_so_far.contains(new_index)) {
+          // var msg = 'First duplicate found for helix ${old_index} with new index ${new_index}. ';
+          // window.alert(msg);
+          // return;
+
+          if (dupe_key_to_idxs.containsKey(new_index))
+            dupe_key_to_idxs[new_index].add(old_index);
+          else
+            dupe_key_to_idxs[new_index] = [ old_index ];
         } else {
-          intsSoFar.add(new_index);
+          ints_so_far.add(new_index);
         }
       }
+
+      if (dupe_key_to_idxs.length != 0) {
+        var msg = 'You tried to assign existing helices ';
+        
+        msg += dupe_key_to_idxs.entries.map((element) {
+          return element.value.join(', ') + " to " + element.key.toString();
+        }).join(" and helices ");
+
+        msg += ". Each helix must have a unique new index; make sure all the integers you write are distinct from each other and do not appear elsewhere in the design";
+
+        window.alert(msg);
+        return;
+      }
+
+
     }
 
     for (int new_idx in action.idx_replacements.values) {
