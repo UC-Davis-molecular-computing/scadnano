75baf80ebfe2774c3426ffc79c22f59d60d107d7
David Doty <doty@ucdavis.edu>
2024-03-28 14:49:43 -0700

    bumped version

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -8,7 +8,7 @@ import 'state/grid.dart';
 
 // WARNING: Do not modify line below, except for the version string
 //          (and also add new version string to scadnano_versions_to_link).
-const String CURRENT_VERSION = "0.19.2";
+const String CURRENT_VERSION = "0.19.3";
 const String INITIAL_VERSION = "0.1.0";
 
 // scadnano versions that we deploy so that older versions can be used.
