ca9272eef5af66555e043dc33864ebe08063598d
dave-doty <dave-doty@users.noreply.github.com>
2024-07-24 02:49:20 +0000

    deploy: ebf29a65ff4a0a7493f868f7b28737cfd9487d67

---
+++ v0.19.3/examples/16_helix_origami_rectangle_shifted_seam_no_seq.py
@@ -0,0 +1,12 @@
+import origami_rectangle as rect
+import scadnano as sc
+
+
+def create_design():
+    design = rect.create(num_helices=16, num_cols=26, assign_seq=False, seam_left_column=2)
+    return design
+
+
+if __name__ == '__main__':
+    design = create_design()
+    design.write_scadnano_file(directory='output_designs')
