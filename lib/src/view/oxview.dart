02aeef092c534d171f16ba53102a1e5288b749d1
David Doty <doty@ucdavis.edu>
2024-09-05 10:01:09 +0100

    Update oxview.dart

--- lib/src/view/oxview.dart
+++ lib/src/view/oxview.dart
@@ -8,16 +8,6 @@ import 'package:codemirror/hints.dart';
 import '../app.dart';
 import '../constants.dart' as constants;
 
-//TODO: compile Python scripts in the browser using pyodide (or something)
-
-/// This should only be created once in the app lifetime, so not much happens in the render() method.
-/// Since it uses the CodeMirror library, it falls a bit outside of the reactive component framework of the
-/// rest of the view and acts more like an outside agent communicating with the rest of the app.
-/// (except for the fact that the app can choose to hide or show it, but it remains "rendered" in the background
-/// and is simply removed from/added to the DOM)
-///
-/// However, other than being hidden/shown, this does not react to any changes happening outside of its element.
-/// The updating of its source code goes through the normal event loop but only involves elements in this component.
 class OxviewViewComponent {
   DivElement root_element;
 
