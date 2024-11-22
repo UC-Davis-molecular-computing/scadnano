8cf86d8ab4165bac2d257277aa3ed01aff1c5070
David Doty <doty@ucdavis.edu>
2024-11-21 19:19:40 -0800

    attempt to fix cadnano export bug (still not fixed)

--- test/export_cadnano_v2_test.dart
+++ test/export_cadnano_v2_test.dart
@@ -297,15 +297,9 @@ main() {
     });
 
     test('test_paranemic_crossover_other_direction', () async {
-      var helices = [
-        Helix(idx: 1, max_offset: 64, grid_position: GridPosition(19, 14), grid: Grid.square),
-        Helix(idx: 0, max_offset: 64, grid_position: GridPosition(19, 15), grid: Grid.square),
-        Helix(idx: 3, max_offset: 64, grid_position: GridPosition(19, 16), grid: Grid.square),
-        Helix(idx: 2, max_offset: 64, grid_position: GridPosition(19, 17), grid: Grid.square),
-      ];
-      var design = Design(helices: helices, grid: Grid.square);
-      design = design.draw_strand(3, 24).to(8).cross(1, 24).to(8).as_scaffold().commit();
-      design = design.draw_strand(2, 50).to(24).cross(0, 50).to(24).commit();
+      String filename = 'test_paranemic_crossover_other_direction.sc';
+      Design design = Design.from_json_str(
+          await get_text_file_content('../test/tests_inputs/cadnano_v2_export/${filename}'))!;
 
       String output_json = to_cadnano_v2_json(design);
       Design output_design = Design.from_cadnano_v2_json_str(output_json);
